d3d81b29f7580b0c81da1cd3924f63c7eca6361b
Сумаренко Екатерина Михайловна <sumarenka@sc.softclub.by>
2022-08-24 18:27:09 +0300

    Change color field

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Cell {
+  i: number;
+  j: number;
+  isCurrent: boolean;
+  isClicked: boolean;
+  isAcess: boolean;
+  step?: number;
+}
+
+function initCell(options?: Partial<Cell>): Cell {
+  const defaults = {
+    i: 1,
+    j: 1,
+    isCurrent: false,
+    isClicked: false,
+    isAcess: false,
+  };
+
+  return {
+    ...defaults,
+    ...options,
+  };
+}
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css']
+  styleUrls: ['./board.component.css'],
 })
 export class BoardComponent implements OnInit {
-  items
+  items: Cell[][];
 
   constructor() {
-    this.items = Array(10).fill(null)
+    this.items = Array(10).fill(Array(10).fill({initCell}));
+  }
+
+  returnCoordinates(a: number, b: number) {
+    alert(a+1 + ',' + (b+1));
+  }
+
+  makeCurrent(cell: Cell) {
+    cell.isClicked = true;
   }
 
   ngOnInit(): void {
