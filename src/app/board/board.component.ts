cf4d3ffc3146d7f7aedc33d1b021d99b33ed0429
Сумаренко Екатерина Михайловна <sumarenka@sc.softclub.by>
2022-08-26 13:39:11 +0300

    Make next cell current only if available

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -47,65 +47,81 @@ export class BoardComponent implements OnInit {
     alert(a+1 + ',' + (b+1));
   }
 
+  clickCount: number = 0;
+  clicked(event: Event) {
+    console.log(event);
+    this.clickCount++;
+  }
+
   makeCurrent(cell: Cell) {
-    for (let i = 0; i < 10; i++) {
-      this.items[i] = [];
-      for (let j = 0; j < 10; j++) {
-        this.items[i][j] = initCell({i, j});
+    const a = cell.i;
+    const b = cell.j;
+
+    if (this.clickCount == 1 || this.items[a][b].isAvailable == true){
+
+      for (let i = 0; i < 10; i++) {
+        this.items[i] = [];
+        for (let j = 0; j < 10; j++) {
+          this.items[i][j] = initCell({i, j});
+        }
+      }
+  
+      const x = cell.i;
+      const y = cell.j;
+      this.items[x][y].isClicked = true;
+  
+      if (x + 2 < this.items.length && y - 1 >= 0) {
+        const x = cell.i + 2;
+        const y = cell.j - 1;
+        this.items[x][y].isAvailable = true;  
+      }
+  
+      if (x + 2 < this.items.length && y + 1 < this.items.length) {
+        const x = cell.i + 2;
+        const y = cell.j + 1;
+        this.items[x][y].isAvailable = true;  
+      }
+  
+      if (x - 2 >= 0 && y - 1 >= 0) {
+        const x = cell.i - 2;
+        const y = cell.j - 1;
+        this.items[x][y].isAvailable = true;  
+      }
+      
+      if (x - 2 >= 0 && y + 1 < this.items.length) {
+        const x = cell.i - 2;
+        const y = cell.j + 1;
+        this.items[x][y].isAvailable = true;  
+      }
+  
+      if (x + 1 < this.items.length && y + 2 < this.items.length) {
+        const x = cell.i + 1;
+        const y = cell.j + 2;
+        this.items[x][y].isAvailable = true;  
+      }
+  
+      if (x - 1 >= 0 && y + 2 < this.items.length) {
+        const x = cell.i - 1;
+        const y = cell.j + 2;
+        this.items[x][y].isAvailable = true;  
+      }
+  
+      if (x + 1 < this.items.length && y - 2 >= 0) {
+        const x = cell.i + 1;
+        const y = cell.j - 2;
+        this.items[x][y].isAvailable = true;  
+      }
+      
+      if (x - 1 >= 0 && y - 2 >= 0) {
+        const x = cell.i - 1;
+        const y = cell.j - 2;
+        this.items[x][y].isAvailable = true;  
       }
     }
 
-    const x = cell.i;
-    const y = cell.j;
-    this.items[x][y].isClicked = true;
+  }
 
-    if (x + 2 < this.items.length && y - 1 >= 0) {
-      const x = cell.i + 2;
-      const y = cell.j - 1;
-      this.items[x][y].isAvailable = true;  
-    }
-
-    if (x + 2 < this.items.length && y + 1 < this.items.length) {
-      const x = cell.i + 2;
-      const y = cell.j + 1;
-      this.items[x][y].isAvailable = true;  
-    }
-
-    if (x - 2 >= 0 && y - 1 >= 0) {
-      const x = cell.i - 2;
-      const y = cell.j - 1;
-      this.items[x][y].isAvailable = true;  
-    }
-    
-    if (x - 2 >= 0 && y + 1 < this.items.length) {
-      const x = cell.i - 2;
-      const y = cell.j + 1;
-      this.items[x][y].isAvailable = true;  
-    }
-
-    if (x + 1 < this.items.length && y + 2 < this.items.length) {
-      const x = cell.i + 1;
-      const y = cell.j + 2;
-      this.items[x][y].isAvailable = true;  
-    }
-
-    if (x - 1 >= 0 && y + 2 < this.items.length) {
-      const x = cell.i - 1;
-      const y = cell.j + 2;
-      this.items[x][y].isAvailable = true;  
-    }
-
-    if (x + 1 < this.items.length && y - 2 >= 0) {
-      const x = cell.i + 1;
-      const y = cell.j - 2;
-      this.items[x][y].isAvailable = true;  
-    }
-    
-    if (x - 1 >= 0 && y - 2 >= 0) {
-      const x = cell.i - 1;
-      const y = cell.j - 2;
-      this.items[x][y].isAvailable = true;  
-    }
+  makeAvailable(cell: Cell) {
   }
 
   ngOnInit(): void {
